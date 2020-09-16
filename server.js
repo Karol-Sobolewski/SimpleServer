8658288bb1ea175b431a0e1aeb1c5656ffed187d
Karol <karolsobolewski92@gmail.com>
2020-09-16 20:05:17 +0200

    Move files to other dir

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+
+const app = express();
+
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main', layout: 'light' }));
+app.set('view engine', '.hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { layout: 'light' });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+    res.render('info');
+});
+
+app.get('/history', (req, res) => {
+    res.render('history');
+});
+
+app.post('/contact/send-message', (req, res) => {
+
+    const { author, sender, title, message, design } = req.body;
+
+    if (author && sender && title && message && design) {
+        res.render('contact', { isSent: true, design: req.params.design});
+    }
+    else {
+        res.render('contact', { isError: true });
+    }
+
+});
+
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
